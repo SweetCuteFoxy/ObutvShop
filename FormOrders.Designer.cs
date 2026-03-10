86f0199431523c6148961bd98044afaeb3605514
SdfSWEET <godlikehokage85@gmail.com>
2026-03-10 13:08:05 +0500

    add product images and filters

--- FormOrders.Designer.cs
+++ FormOrders.Designer.cs
@@ -16,82 +16,80 @@ namespace ObutvShop
         private void InitializeComponent()
         {
             panelTop = new Panel();
+            panelHeader = new Panel();
             labelTitle = new Label();
             labelUserInfo = new Label();
-            labelSearch = new Label();
+            panelFilters = new Panel();
             textBoxSearch = new TextBox();
-            labelFilterStatus = new Label();
             comboBoxStatus = new ComboBox();
+            splitContainer = new SplitContainer();
             dataGridViewOrders = new DataGridView();
+            panelItemsHeader = new Panel();
+            labelItems = new Label();
+            dataGridViewItems = new DataGridView();
             panelBottom = new Panel();
             labelCount = new Label();
-            splitContainer = new SplitContainer();
-            dataGridViewItems = new DataGridView();
-            labelItems = new Label();
-            panelItemsHeader = new Panel();
 
             panelTop.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridViewOrders).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridViewItems).BeginInit();
+            panelHeader.SuspendLayout();
+            panelFilters.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)splitContainer).BeginInit();
             splitContainer.Panel1.SuspendLayout();
             splitContainer.Panel2.SuspendLayout();
             splitContainer.SuspendLayout();
-            panelBottom.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewOrders).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewItems).BeginInit();
             panelItemsHeader.SuspendLayout();
+            panelBottom.SuspendLayout();
             SuspendLayout();
 
             // panelTop
-            panelTop.BackColor = Color.FromArgb(127, 255, 0);
-            panelTop.Controls.Add(labelTitle);
-            panelTop.Controls.Add(labelUserInfo);
-            panelTop.Controls.Add(labelSearch);
-            panelTop.Controls.Add(textBoxSearch);
-            panelTop.Controls.Add(labelFilterStatus);
-            panelTop.Controls.Add(comboBoxStatus);
+            panelTop.BackColor = Color.White;
+            panelTop.Controls.Add(panelFilters);
+            panelTop.Controls.Add(panelHeader);
             panelTop.Dock = DockStyle.Top;
-            panelTop.Name = "panelTop";
-            panelTop.Size = new Size(1264, 100);
+            panelTop.Padding = new Padding(24, 0, 24, 0);
+            panelTop.Size = new Size(1264, 115);
+
+            // panelHeader
+            panelHeader.Dock = DockStyle.Top;
+            panelHeader.Controls.Add(labelUserInfo);
+            panelHeader.Controls.Add(labelTitle);
+            panelHeader.Size = new Size(1216, 55);
 
             // labelTitle
+            labelTitle.Dock = DockStyle.Left;
             labelTitle.Font = new Font("Times New Roman", 18F, FontStyle.Bold);
-            labelTitle.Location = new Point(20, 10);
-            labelTitle.Size = new Size(250, 35);
+            labelTitle.ForeColor = Color.FromArgb(46, 139, 87);
+            labelTitle.Size = new Size(200, 55);
             labelTitle.Text = "Заказы";
+            labelTitle.TextAlign = ContentAlignment.MiddleLeft;
 
             // labelUserInfo
-            labelUserInfo.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            labelUserInfo.Font = new Font("Times New Roman", 11F);
-            labelUserInfo.Location = new Point(900, 10);
-            labelUserInfo.Size = new Size(350, 30);
-            labelUserInfo.Text = "Гость";
+            labelUserInfo.Dock = DockStyle.Right;
+            labelUserInfo.Font = new Font("Times New Roman", 10F);
+            labelUserInfo.ForeColor = Color.Gray;
+            labelUserInfo.Size = new Size(300, 55);
             labelUserInfo.TextAlign = ContentAlignment.MiddleRight;
 
-            // labelSearch
-            labelSearch.Font = new Font("Times New Roman", 11F);
-            labelSearch.Location = new Point(20, 60);
-            labelSearch.Size = new Size(60, 25);
-            labelSearch.Text = "Поиск:";
+            // panelFilters
+            panelFilters.Dock = DockStyle.Bottom;
+            panelFilters.Controls.Add(comboBoxStatus);
+            panelFilters.Controls.Add(textBoxSearch);
+            panelFilters.Size = new Size(1216, 45);
 
             // textBoxSearch
+            textBoxSearch.BorderStyle = BorderStyle.FixedSingle;
             textBoxSearch.Font = new Font("Times New Roman", 11F);
-            textBoxSearch.Location = new Point(85, 57);
-            textBoxSearch.Name = "textBoxSearch";
+            textBoxSearch.Location = new Point(0, 8);
             textBoxSearch.Size = new Size(300, 28);
-            textBoxSearch.PlaceholderText = "Номер заказа или клиент";
+            textBoxSearch.PlaceholderText = "Поиск по номеру или клиенту...";
             textBoxSearch.TextChanged += TextBoxSearch_TextChanged;
 
-            // labelFilterStatus
-            labelFilterStatus.Font = new Font("Times New Roman", 11F);
-            labelFilterStatus.Location = new Point(420, 60);
-            labelFilterStatus.Size = new Size(65, 25);
-            labelFilterStatus.Text = "Статус:";
-
             // comboBoxStatus
-            comboBoxStatus.Font = new Font("Times New Roman", 11F);
+            comboBoxStatus.Font = new Font("Times New Roman", 10F);
             comboBoxStatus.DropDownStyle = ComboBoxStyle.DropDownList;
-            comboBoxStatus.Location = new Point(490, 57);
-            comboBoxStatus.Name = "comboBoxStatus";
+            comboBoxStatus.Location = new Point(315, 8);
             comboBoxStatus.Size = new Size(200, 28);
             comboBoxStatus.SelectedIndexChanged += ComboBoxStatus_SelectedIndexChanged;
 
@@ -99,7 +97,8 @@ namespace ObutvShop
             splitContainer.Dock = DockStyle.Fill;
             splitContainer.Orientation = Orientation.Horizontal;
             splitContainer.SplitterDistance = 350;
-            splitContainer.Name = "splitContainer";
+            splitContainer.SplitterWidth = 6;
+            splitContainer.BackColor = Color.FromArgb(235, 235, 235);
 
             // splitContainer.Panel1 - orders grid
             splitContainer.Panel1.Controls.Add(dataGridViewOrders);
@@ -112,36 +111,48 @@ namespace ObutvShop
             dataGridViewOrders.AllowUserToAddRows = false;
             dataGridViewOrders.AllowUserToDeleteRows = false;
             dataGridViewOrders.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridViewOrders.BackgroundColor = Color.White;
+            dataGridViewOrders.BackgroundColor = Color.FromArgb(250, 250, 250);
             dataGridViewOrders.BorderStyle = BorderStyle.None;
             dataGridViewOrders.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
-            dataGridViewOrders.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
-            dataGridViewOrders.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(0, 250, 154);
-            dataGridViewOrders.ColumnHeadersDefaultCellStyle.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
-            dataGridViewOrders.ColumnHeadersDefaultCellStyle.ForeColor = Color.Black;
-            dataGridViewOrders.ColumnHeadersDefaultCellStyle.SelectionBackColor = Color.FromArgb(0, 250, 154);
-            dataGridViewOrders.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewOrders.DefaultCellStyle.Font = new Font("Times New Roman", 10F);
-            dataGridViewOrders.DefaultCellStyle.SelectionBackColor = Color.FromArgb(200, 255, 200);
-            dataGridViewOrders.DefaultCellStyle.SelectionForeColor = Color.Black;
+            dataGridViewOrders.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewOrders.ColumnHeadersDefaultCellStyle = new DataGridViewCellStyle
+            {
+                BackColor = Color.FromArgb(245, 245, 245),
+                Font = new Font("Times New Roman", 10F, FontStyle.Bold),
+                ForeColor = Color.FromArgb(80, 80, 80),
+                SelectionBackColor = Color.FromArgb(245, 245, 245),
+                SelectionForeColor = Color.FromArgb(80, 80, 80),
+                Padding = new Padding(8, 4, 8, 4)
+            };
+            dataGridViewOrders.ColumnHeadersHeight = 40;
+            dataGridViewOrders.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridViewOrders.DefaultCellStyle = new DataGridViewCellStyle
+            {
+                Font = new Font("Times New Roman", 10F),
+                ForeColor = Color.FromArgb(50, 50, 50),
+                SelectionBackColor = Color.FromArgb(232, 245, 233),
+                SelectionForeColor = Color.Black,
+                Padding = new Padding(8, 2, 8, 2)
+            };
             dataGridViewOrders.Dock = DockStyle.Fill;
             dataGridViewOrders.EnableHeadersVisualStyles = false;
-            dataGridViewOrders.GridColor = Color.FromArgb(220, 220, 220);
-            dataGridViewOrders.Name = "dataGridViewOrders";
+            dataGridViewOrders.GridColor = Color.FromArgb(235, 235, 235);
             dataGridViewOrders.ReadOnly = true;
             dataGridViewOrders.RowHeadersVisible = false;
-            dataGridViewOrders.RowTemplate.Height = 30;
+            dataGridViewOrders.RowTemplate.Height = 35;
             dataGridViewOrders.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             dataGridViewOrders.SelectionChanged += DataGridViewOrders_SelectionChanged;
 
             // panelItemsHeader
+            panelItemsHeader.BackColor = Color.FromArgb(245, 245, 245);
             panelItemsHeader.Dock = DockStyle.Top;
-            panelItemsHeader.Size = new Size(1264, 30);
+            panelItemsHeader.Size = new Size(1264, 36);
             panelItemsHeader.Controls.Add(labelItems);
 
             // labelItems
             labelItems.Dock = DockStyle.Fill;
             labelItems.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
+            labelItems.ForeColor = Color.FromArgb(46, 139, 87);
             labelItems.Text = "  Состав заказа";
             labelItems.TextAlign = ContentAlignment.MiddleLeft;
 
@@ -149,71 +160,92 @@ namespace ObutvShop
             dataGridViewItems.AllowUserToAddRows = false;
             dataGridViewItems.AllowUserToDeleteRows = false;
             dataGridViewItems.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridViewItems.BackgroundColor = Color.FromArgb(250, 250, 250);
+            dataGridViewItems.BackgroundColor = Color.White;
             dataGridViewItems.BorderStyle = BorderStyle.None;
             dataGridViewItems.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
-            dataGridViewItems.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
-            dataGridViewItems.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(200, 230, 200);
-            dataGridViewItems.ColumnHeadersDefaultCellStyle.Font = new Font("Times New Roman", 10F, FontStyle.Bold);
-            dataGridViewItems.ColumnHeadersDefaultCellStyle.ForeColor = Color.Black;
-            dataGridViewItems.ColumnHeadersDefaultCellStyle.SelectionBackColor = Color.FromArgb(200, 230, 200);
-            dataGridViewItems.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewItems.DefaultCellStyle.Font = new Font("Times New Roman", 10F);
+            dataGridViewItems.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewItems.ColumnHeadersDefaultCellStyle = new DataGridViewCellStyle
+            {
+                BackColor = Color.FromArgb(245, 245, 245),
+                Font = new Font("Times New Roman", 10F, FontStyle.Bold),
+                ForeColor = Color.FromArgb(80, 80, 80),
+                SelectionBackColor = Color.FromArgb(245, 245, 245),
+                SelectionForeColor = Color.FromArgb(80, 80, 80)
+            };
+            dataGridViewItems.ColumnHeadersHeight = 36;
+            dataGridViewItems.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridViewItems.DefaultCellStyle = new DataGridViewCellStyle
+            {
+                Font = new Font("Times New Roman", 10F),
+                ForeColor = Color.FromArgb(50, 50, 50),
+                SelectionBackColor = Color.FromArgb(240, 248, 240),
+                SelectionForeColor = Color.Black
+            };
             dataGridViewItems.Dock = DockStyle.Fill;
             dataGridViewItems.EnableHeadersVisualStyles = false;
-            dataGridViewItems.GridColor = Color.FromArgb(220, 220, 220);
-            dataGridViewItems.Name = "dataGridViewItems";
+            dataGridViewItems.GridColor = Color.FromArgb(235, 235, 235);
             dataGridViewItems.ReadOnly = true;
             dataGridViewItems.RowHeadersVisible = false;
-            dataGridViewItems.RowTemplate.Height = 28;
+            dataGridViewItems.RowTemplate.Height = 30;
             dataGridViewItems.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
 
             // panelBottom
-            panelBottom.BackColor = Color.FromArgb(127, 255, 0);
+            panelBottom.BackColor = Color.White;
             panelBottom.Controls.Add(labelCount);
             panelBottom.Dock = DockStyle.Bottom;
-            panelBottom.Size = new Size(1264, 35);
+            panelBottom.Size = new Size(1264, 38);
 
             // labelCount
             labelCount.Dock = DockStyle.Fill;
             labelCount.Font = new Font("Times New Roman", 10F);
-            labelCount.Padding = new Padding(20, 0, 20, 0);
-            labelCount.Text = "0 из 0";
+            labelCount.ForeColor = Color.Gray;
+            labelCount.Padding = new Padding(24, 0, 24, 0);
+            labelCount.Text = "Показано 0 из 0 заказов";
+            labelCount.TextAlign = ContentAlignment.MiddleLeft;
 
             // FormOrders
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.White;
+            BackColor = Color.FromArgb(246, 246, 246);
             ClientSize = new Size(1264, 681);
             Controls.Add(splitContainer);
             Controls.Add(panelBottom);
             Controls.Add(panelTop);
             Name = "FormOrders";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Заказы";
+            Text = "Заказы - Магазин обуви";
             WindowState = FormWindowState.Maximized;
 
+            var iconPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "Icon.ico");
+            if (File.Exists(iconPath))
+            {
+                using var bmp = new Bitmap(iconPath);
+                Icon = System.Drawing.Icon.FromHandle(bmp.GetHicon());
+            }
+
             panelTop.ResumeLayout(false);
-            panelTop.PerformLayout();
+            panelHeader.ResumeLayout(false);
+            panelFilters.ResumeLayout(false);
+            panelFilters.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridViewOrders).EndInit();
             ((System.ComponentModel.ISupportInitialize)dataGridViewItems).EndInit();
             splitContainer.Panel1.ResumeLayout(false);
             splitContainer.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)splitContainer).EndInit();
             splitContainer.ResumeLayout(false);
-            panelBottom.ResumeLayout(false);
             panelItemsHeader.ResumeLayout(false);
+            panelBottom.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panelTop;
+        private Panel panelHeader;
         private Label labelTitle;
         private Label labelUserInfo;
-        private Label labelSearch;
+        private Panel panelFilters;
         private TextBox textBoxSearch;
-        private Label labelFilterStatus;
         private ComboBox comboBoxStatus;
         private SplitContainer splitContainer;
         private DataGridView dataGridViewOrders;
